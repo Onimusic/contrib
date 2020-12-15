8e073c7dada99c71608eaad6f19638bd8c467d8f
Daniel <daniel.santana@onimusic.com.br>
2020-12-15 12:13:36 -0300

    removendo apps.py e management

--- apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContribConfig(AppConfig):
-    name = 'django_boilerplate.apps.contrib'
-    label = 'contrib'
--- management/commands/batima.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    help = 'Temp'
-
-    def handle(self, **other):
-        pass
